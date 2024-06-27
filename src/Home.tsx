a0dc85f8b783a9cd53043c100249263fdc0338e4
Kevin Ibanez <kevin.ibanez.dev@gmail.com>
2024-06-27 16:57:55 -0500

    Updated Landing screen

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,11 +3,10 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center gap-4">
-        <h1 className="lg:text-7xl font-bold">
-          Ibanez Kanban Web App by <span>Kevin Ibanez</span>
-        </h1>
-        <p className="lg:text-3xl w-3/4 pb-5">
+      <div className="flex flex-col items-center gap-4 px-2">
+        <h1 className="text-6xl md:text-7xl font-bold">Ibanez Kanban</h1>
+        <h3 className="text-3xl">by Kevin Ibanez</h3>
+        <p className="md:text-2xl px-3 text-center">
           Choose <b>Demo</b> for a Kanban experience with pre-defined data or
           <b className="text-typography-purple"> Login</b> to the personalized
           experience!
@@ -15,13 +14,13 @@ const Home = () => {
         <div className="flex items-center gap-4">
           <Link
             to="/demo"
-            className="flex items-center justify-center w-40 text-4xl bg-black text-white rounded-2xl h-16"
+            className="flex items-center justify-center w-28 md:w-40 text-3xl md:text-4xl bg-black text-white rounded-2xl h-16"
           >
             Demo
           </Link>
           <Link
             to="/login"
-            className="w-40 flex items-center justify-center text-4xl bg-typography-purple text-white rounded-2xl h-16"
+            className="flex items-center justify-center w-28 md:w-40 text-3xl md:text-4xl bg-typography-purple text-white rounded-2xl h-16"
           >
             Kanban
           </Link>
