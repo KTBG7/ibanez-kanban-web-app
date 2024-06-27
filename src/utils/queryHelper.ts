85345afe325d9515c0b428fb6f16ae15b77b37e0
Kevin Ibanez <kevin.ibanez.dev@gmail.com>
2024-06-27 16:37:47 -0500

    Setting up vite env variables

--- src/utils/queryHelper.ts
+++ src/utils/queryHelper.ts
@@ -1,76 +1,62 @@
 import { Boards } from '../types/GlobalTypes';
+export const API_URL = import.meta.env.VITE_KANBAN_BACKEND_URL;
 
 export const userLogin = async (email: string, password: string) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_KANBAN_BACKEND_URL}/login`,
-    {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
+  const response = await fetch(`${API_URL}/login`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  );
+    body: JSON.stringify({ email, password }),
+  });
   return response.json();
 };
 
 export const userSignUp = async (email: string, password: string) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_KANBAN_BACKEND_URL}/signup`,
-    {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
+  const response = await fetch(`${API_URL}/signup`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  );
+    body: JSON.stringify({ email, password }),
+  });
   return response.json();
 };
 
 export const getBoards = async (user: string) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_KANBAN_BACKEND_URL}/boards`,
-    {
-      method: 'GET',
-      credentials: 'include',
-      headers: {
-        'x-csrf-token': user,
-      },
+  const response = await fetch(`${API_URL}/boards`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'x-csrf-token': user,
     },
-  );
+  });
   return response.json();
 };
 
 export const postBoards = async (boards: Boards, user: string) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_KANBAN_BACKEND_URL}/update-user-boards`,
-    {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-csrf-token': user,
-      },
-      body: JSON.stringify({ boards }),
+  const response = await fetch(`${API_URL}/update-user-boards`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-csrf-token': user,
     },
-  );
+    body: JSON.stringify({ boards }),
+  });
   return response.json();
 };
 
 export const postUserLogout = async (user: string) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_KANBAN_BACKEND_URL}/logout`,
-    {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-csrf-token': user,
-      },
+  const response = await fetch(`${API_URL}/logout`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-csrf-token': user,
     },
-  );
+  });
   return response.json();
 };
